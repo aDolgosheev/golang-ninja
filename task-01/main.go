c1f7d5ae01ccdcc695ca61a629038527052b716f
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-11-21 23:38:39 +0300

    initial commit

---
+++ task-01/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var message string
+	var number float32
+	var b bool
+
+	number = 12.2
+	b = true
+
+	fmt.Println(message)
+	fmt.Println(number)
+	fmt.Println(b)
+}
