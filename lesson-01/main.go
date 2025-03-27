527a6dc700a74a7566eb7e779cc8db4e4bb506f5
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2025-03-27 17:21:34 +0300

    2025.03.27 initial commit

---
+++ lesson-01/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var message string = "Hello Golang ninja!"
+	// message := "Hello Golang ninja!"
+
+	/*
+		asf
+		asf
+		asf
+	*/
+
+	// fmt.Println(message)
+
+
+	var message string
+	var number int
+	var b bool
+
+	fmt.Println(message)
+	fmt.Println(number)
+	fmt.Println(b)
+}
---
+++ lesson-02/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var a, b, c int
+
+func main() {
+	fmt.Println(sayHello("Максим", 13))
+	printMessage("вызов 1")
+	printMessage("вызов 2")
+	printMessage("вызов 3")
+
+	message, err := enterTheClub(18)
+	if err != nil {
+		
+	}
+}
+
+func printMessage(message string) {
+	fmt.Println(message)
+}
+
+func sayHello(name string, age int) string {
+	return fmt.Sprintf("Привет, %s! Тебе %d лет.", name, age)
+}
+
+// func enterTheClub(age int) (string, bool) {
+// 	if age >= 18 {
+// 		return "Входи, только аккуратно.", true
+// 	} else if age >= 45 {
+// 		return "Вам точно тут понравится?", true
+// 	} 
+// 	return "Тебе еще рано.", false
+// }
+
+func enterTheClub(age int) (string, error) {
+	if age >= 18 {
+		return "Входи, только аккуратно.", nil
+	} else if age >= 45 {
+		return "Вам точно тут понравится?", errors.New("Староваты все-таки")
+	} 
+	return "Тебе еще рано.", errors.New("Подрасти, малой")
+}
