e1ec77b0331acdf6dd2366e24d93d511dadf43d0
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2025-07-24 23:24:25 +0300

    2025.07.24

--- lesson-04/main.go
+++ lesson-04/main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
 // var msg string
 
@@ -12,43 +9,18 @@ import (
 // }
 
 func main() {
-	// fmt.Println(msg)
-
-	// var p *int
 	// message := "Скоро я стану ниндзя!"
-	// fmt.Println(message)
 
 	// changeMessage(&message)
-
 	// fmt.Println(message)
 
-	// messages := [3]string{"1", "2", "3"}
-	// fmt.Println(messages)
-	// printMessages(messages)
-	// fmt.Println(messages)
-
-	// messages := []string{"1", "2", "3"}
-
-	messages := make([]string, 5)
-
-	messages = append(messages, "6")
-
-	// printMessages(messages)
+	messages := [3]string {"1", "2", "3"}
 	fmt.Println(messages)
-}
-
-func printMessages(messages []string) error {
-	if len(messages) == 0 {
-		return errors.New("empty array")
-	}
-
-	messages[1] = "5"
-
-	fmt.Println(messages)
-
-	return nil
+	fmt.Println(messages[1])
 }
 
 // func changeMessage(message *string) {
 // 	*message += " (из функции changeMessage())"
 // }
+
+
