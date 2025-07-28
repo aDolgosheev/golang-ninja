2e6c5acd442048bcb6b30bdc359425f553396d97
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2025-07-28 17:20:42 +0300

    2025.07.28 update

--- lesson-04/main.go
+++ lesson-04/main.go
@@ -2,25 +2,87 @@ package main
 
 import "fmt"
 
-// var msg string
-
-// func init() {
-// 	msg = "from init()"
-// }
-
 func main() {
-	// message := "Скоро я стану ниндзя!"
+	messages := []string{
+		"message 1",
+		"message 2",
+		"message 3",
+		"message 4",
+	}
 
-	// changeMessage(&message)
-	// fmt.Println(message)
-
-	messages := [3]string {"1", "2", "3"}
-	fmt.Println(messages)
-	fmt.Println(messages[1])
+	for i := range messages {
+		fmt.Println(i)
+		fmt.Println(messages[i])
+	}
 }
 
-// func changeMessage(message *string) {
-// 	*message += " (из функции changeMessage())"
+// import (
+// 	"errors"
+// 	"fmt"
+// )
+
+// // var msg string
+
+// // func init() {
+// // 	msg = "from init()"
+// // }
+
+// func main() {
+// 	// message := "Скоро я стану ниндзя!"
+
+// 	// changeMessage(&message)
+// 	// fmt.Println(message)
+
+// 	// messages := [3]string {"1", "2", "3"}
+
+// 	// messages := []string {"1", "2", "3"}
+
+// 	// var messages []string
+// 	// messages[0] = "1"
+// 	// fmt.Println(messages)
+// 	// fmt.Println(messages[1])
+// 	// printMessage(messages)
+// 	// fmt.Println(messages)
+
+// 	// matrix := make([][]int, 10)
+
+// 	// for x := 0; x < 10; x++ {
+// 	// 	for y := 0; y < 10; y++ {
+// 	// 		matrix[y] = make([]int, 10)
+// 	// 		matrix[y][x] = x
+// 	// 	}
+// 	// 	fmt.Println(matrix[x])
+// 	// }
+
+// 	matrix := make([][]int, 10)
+
+// 	counter := 0
+// 	for x := 0; x < 10; x++ {
+// 		matrix[x] = make([]int, 10)
+// 		for y := 0; y < 10; y++ {
+// 			counter++
+// 			matrix[x][y] = counter
+// 		}
+// 		fmt.Println(matrix[x])
+// 	}
+
+// 	// for x := 0; true; x++ {
+// 	// 	fmt.Println(x)
+// 	// }
 // }
 
+// func printMessage(messages []string) error {
+// 	if len(messages) == 0 {
+// 		return errors.New("empty array")
+// 	}
 
+// 	messages[1] = "5"
+
+// 	fmt.Println(messages)
+
+// 	return nil
+// }
+
+// // func changeMessage(message *string) {
+// // 	*message += " (из функции changeMessage())"
+// // }
