2af2a9606074169021e671ac87caef1df2465890
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2025-09-30 23:38:55 +0300

    2025.09.30 update

--- lesson-18/main.go
+++ lesson-18/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 func main() {
 	ctx := context.Background()
-	ctx, _ 
+	ctx, _ = context.WithTimeout(ctx, time.Second)
+}
+
+func parse(ctx context.Context) {
+	for {
+		select {
+		case <-time.After(time.Second * 2):
+
+		case <-ctx.Done():
+			fmt.Println("deadline exceeded")
+			return
+		}
+	}
 }
